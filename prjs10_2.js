6ae8190c3d34720d2d5f0e6b255d0106ad1c1397
Himalaya <yugmani@yahoo.com>
2020-04-29 23:12:26 -0700

    updated the event listeners

---
+++ prjs10_2.js
@@ -0,0 +1,8 @@
+let int = document.getElementById("int");
+int.addEventListener("mouseover", function(){
+    int.style.color = "#FF0000";
+}, false);
+
+int.addEventListener("mouseout", function(){
+    int.style.fontSize = "20px";
+}, false);
